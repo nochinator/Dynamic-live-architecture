75d7dd0b09b0a47bfa4575f251ec0d4fdaae7240
nochinator <3nochinator@proton.me>
2024-01-22 09:45:24 -0500

    Re-implemented cpu usage as fallback from openCL. automagicaly uses multi-core cpus to their fullest extent. Currently cannot cap the processing speed (should be fixed before realeasing to beta)

--- network_manager.py
+++ network_manager.py
@@ -1,6 +1,14 @@
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+from typing import List
 import pickle
 import neuron as n
 from typing import List, Union
+try:
+    import pyopencl as cl
+    opencl_available = True
+except ImportError:
+    opencl_available = False
+import numpy as np
 
 
 class NeuralNetwork:
@@ -16,39 +24,117 @@ class NeuralNetwork:
         self.hidden_layers = hidden_layers
         self.output_neurons = output_neurons
 
-    def propagate_input(self, inputs: List[float]) -> List[float]:
-        """
-        Provide inputs for the entire network and propagate them through the entire network.
-        :param inputs: array of shape *number of input neurons*
-        :return: network outputs
-        """
-        outputs = []
+    # use open cl if its avalible
+    if opencl_available:
+        def propagate_input(self, inputs: List[float]) -> List[float]:
+            """
+            Provide inputs for the entire network and propagate them through the entire network.
+            :param inputs: array of shape *number of input neurons*
+            :return: network outputs
+            """
 
-        # Prime neurons
-        for i, neuron in enumerate(self.input_neurons):
-            neuron.prime(inputs[i])
-        # Fire input neurons
-        for neuron in self.input_neurons:
-            neuron.fire()
+            outputs = []
 
-        # Hidden neurons layer by layer
-        for layer in self.hidden_layers:
-            # Prime neurons in the layer
-            for neuron in layer:
-                neuron.prime()
-            # Fire neurons in the layer
-            for neuron in layer:
+            # Set up OpenCL context and command queue
+            platform = cl.get_platforms()[0]
+            device = platform.get_devices()[0]
+            context = cl.Context([device])
+            queue = cl.CommandQueue(context)
+
+            # Create buffers for inputs and outputs
+            inputs_buffer = cl.Buffer(context, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR,
+                                      hostbuf=np.array(inputs, dtype=np.float32))
+            outputs_buffer = cl.Buffer(context, cl.mem_flags.WRITE_ONLY, size=np.array(outputs, dtype=np.float32).nbytes)
+
+            # Prime input neurons
+            for i, neuron in enumerate(self.input_neurons):
+                neuron.prime(inputs[i])
+
+            # Fire input neurons
+            for neuron in self.input_neurons:
                 neuron.fire()
 
-        # Output neurons
-        for neuron in self.output_neurons:
-            # Prime output neuron
-            neuron.prime()
-        for neuron in self.output_neurons:
-            # Fire output neuron
-            neuron.fire()
-            outputs.append(neuron.output)
-        return outputs
+            # Hidden neurons layer by layer
+            for layer in self.hidden_layers:
+                # Prime neurons in the layer
+                for neuron in layer:
+                    neuron.prime()
+
+                # Fire neurons in the layer using OpenCL
+                program_code = """
+                    __kernel void fire(__global float* inputs, __global float* outputs) {
+                        int id = get_global_id(0);
+                        outputs[id] = tanh(inputs[id]);  // Adjust activation function as needed
+                    }
+                """
+                program = cl.Program(context, program_code).build()
+                layer_inputs = np.array([neuron.output for neuron in layer], dtype=np.float32)
+                layer_inputs_buffer = cl.Buffer(context, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR,
+                                                hostbuf=layer_inputs)
+                program.fire(queue, layer_inputs.shape, None, layer_inputs_buffer, outputs_buffer).wait()
+
+                # Update neuron outputs
+                outputs = np.empty_like(layer_inputs)
+                cl.enqueue_copy(queue, outputs, outputs_buffer).wait()
+                for i, neuron in enumerate(layer):
+                    neuron.output = outputs[i]
+
+            # Output neurons
+            for neuron in self.output_neurons:
+                # Prime output neuron
+                neuron.prime()
+
+            # Fire output neurons using OpenCL
+            program_code = """
+                __kernel void fire(__global float* inputs, __global float* outputs) {
+                    int id = get_global_id(0);
+                    outputs[id] = tanh(inputs[id]);  // Adjust activation function as needed
+                }
+            """
+            program = cl.Program(context, program_code).build()
+            output_inputs = np.array([neuron.output for neuron in self.output_neurons], dtype=np.float32)
+            output_inputs_buffer = cl.Buffer(context, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR,
+                                             hostbuf=output_inputs)
+            program.fire(queue, output_inputs.shape, None, output_inputs_buffer, outputs_buffer).wait()
+
+            # Update neuron outputs
+            cl.enqueue_copy(queue, outputs, outputs_buffer).wait()
+            for i, neuron in enumerate(self.output_neurons):
+                neuron.output = outputs[i]
+                outputs.append(neuron.output)
+
+    # otherwise use multi-core cpu processing as fallback
+    else:
+        def propagate_input(self, inputs: List[float]) -> List[float]:
+            outputs = []
+
+            with ThreadPoolExecutor() as priming_executor:
+                # Prime input neurons using a thread pool
+                priming_executor.map(lambda x, y: x.prime(y), self.input_neurons, inputs)
+
+            with ThreadPoolExecutor() as firing_executor:
+                # Fire input neurons using a thread pool
+                firing_executor.map(lambda x: x.fire(), self.input_neurons)
+
+            for layer in self.hidden_layers:
+                with ProcessPoolExecutor() as priming_executor:
+                    # Prime neurons in the layer using a process pool
+                    priming_executor.map(lambda x: x.prime(), layer)
+
+                with ProcessPoolExecutor() as firing_executor:
+                    # Fire neurons in the layer using a process pool
+                    firing_executor.map(lambda x: x.fire(), layer)
+
+            with ThreadPoolExecutor() as priming_executor:
+                # Prime output neurons using a thread pool
+                priming_executor.map(lambda x: x.prime(), self.output_neurons)
+
+            with ThreadPoolExecutor() as firing_executor:
+                # Fire output neurons using a thread pool
+                firing_executor.map(lambda x: x.fire(), self.output_neurons)
+                outputs.extend(neuron.output for neuron in self.output_neurons)
+
+            return outputs
 
     def reinforce(self, reward: List[float], backpropagations: int) -> None:
         """
